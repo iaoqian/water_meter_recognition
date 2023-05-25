1d4c5529a13877c46ea3c1c7e6a1c902e249ecf4
Kai Wang <1602708828@qq.com>
2023-05-25 20:47:23 +0800

    Add files via upload

---
+++ models/RotationRegressionNetwork.py
@@ -0,0 +1,133 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import PIL.Image
+
+
+class Transform(torch.nn.Module):
+    def __init__(self, img_transforms=None):
+        super().__init__()
+        self.origin_sizes = []
+
+        if img_transforms is not None:
+            self.img_transforms = img_transforms
+        else:
+            self.img_transforms = self._default_transforms
+
+    def _default_transforms(self, image, angels=None):
+        # input image with size (W, H)
+        if isinstance(image, PIL.Image.Image):
+            image = transforms.ToTensor()(image)
+
+        origin_size = image.shape[1: 3]
+
+        self.origin_sizes.append(origin_size)
+        if origin_size[0] / origin_size[1] >= 1.5:
+            image = transforms.Resize((480, 270))(image)
+            if angels:
+                angels = torch.atan(
+                    torch.tan(angels * 90 / 180 * torch.pi) *
+                    (origin_size[0] / 480.0) / (origin_size[1] / 270.0)
+                ) / torch.pi * 180.0 / 90.0
+        elif origin_size[0] / origin_size[1] <= 0.67:
+            image = transforms.Resize((270, 480))(image)
+            if angels:
+                angels = torch.atan(
+                    torch.tan(angels * 90 / 180 * torch.pi) *
+                    (origin_size[0] / 270.0) / (origin_size[1] / 480.0)
+                ) / torch.pi * 180.0 / 90.0
+        else:
+            image = transforms.Resize((270, 270))(image)
+
+        if angels is not None:
+            return transforms.CenterCrop((224, 224))(image), angels
+        else:
+            return transforms.CenterCrop((224, 224))(image)
+
+    def forward(self, images: torch.Tensor or tuple or list, angels=None):
+        if isinstance(images, torch.Tensor):
+            images = [images]
+        if isinstance(images, tuple):
+            images = list(images)
+        if isinstance(angels, torch.Tensor):
+            angels = [angels]
+        if isinstance(angels, tuple):
+            angels = list(angels)
+        if angels is not None:
+            for i in range(len(images)):
+                images[i], angels[i] = self.img_transforms(images[i], angels[i])
+        else:
+            for i in range(len(images)):
+                images[i] = self.img_transforms(images[i])
+        # convert to 4-D things
+        if angels is not None:
+            return torch.stack(images, dim=0), torch.cat(angels, dim=0)
+        else:
+            return torch.stack(images, dim=0), None
+
+    def post_process(self, predictions: torch.Tensor):
+        # model's outputs are normalized angles(in degree)
+        # this method returns real rotation angles in origin images
+        predictions = torch.tan(predictions * 90.0 / 180.0 * torch.pi)
+
+        for i in range(predictions.shape[0]):
+            origin_size = self.origin_sizes[i]
+
+            if origin_size[0] / origin_size[1] >= 1.5:
+                predictions[i] = torch.atan(
+                    torch.tan(predictions[i] * 90 / 180 * torch.pi) /
+                    (origin_size[0] / 480.0) * (origin_size[1] / 270.0)
+                )
+            elif origin_size[0] / origin_size[1] <= 0.67:
+                predictions[i] = torch.atan(
+                    torch.tan(predictions[i] * 90 / 180 * torch.pi) /
+                    (origin_size[0] / 270.0) * (origin_size[1] / 480.0)
+                )
+        self.origin_sizes.clear()
+        return torch.atan(predictions) / torch.pi * 180.0
+
+
+class RotaRegNetwork(torch.nn.Module):
+    # Rotation Angle Regression Detection
+    def __init__(self, transform, backbone, output_dim):
+        super().__init__()
+        self.transform = transform
+        self.backbone = backbone
+        self.reg_head = torch.nn.Linear(output_dim, 1)
+
+    @staticmethod
+    def compute_loss(y_hat, y):
+        # compute RMSE loss
+        mse_loss = torch.nn.MSELoss()(y_hat, y)
+        mae_loss = torch.nn.L1Loss()(y_hat, y)
+        smooth_l1_loss = torch.nn.SmoothL1Loss()(y_hat, y)
+        return {'mse_loss': mse_loss, 'mae_loss': mae_loss, 'smooth_l1_loss': smooth_l1_loss}
+
+    def forward(self, images, rota_values=None):
+        # TODO not proper usage
+        device = self.reg_head._parameters['bias'].device
+        X, y = self.transform(images, rota_values)
+        X = X.to(device)
+        y = y.to(device) if y is not None else None
+        y_hat = self.reg_head(self.backbone(X))
+        if self.training:
+            assert y is not None, 'rota_values can\'t be None when model in training mode.'
+            return RotaRegNetwork.compute_loss(y_hat, y), self.transform.post_process(y_hat)
+        else:
+            return self.transform.post_process(y_hat)
+
+
+def rota_reg_net_resnet18(pretrained_model_filepath=None):
+    if not pretrained_model_filepath:
+        backbone = torchvision.models.resnet18()
+    else:
+        backbone = torch.load(pretrained_model_filepath)
+
+    backbone.fc = torch.nn.Identity()
+
+    config = {
+        'backbone': backbone,
+        'output_dim': 512,
+        'transform': Transform()
+    }
+    return RotaRegNetwork(**config)
---
+++ models/SingleObjectDetectionNetwork.py
@@ -0,0 +1,94 @@
+import torch
+import torchvision
+
+
+class SingleObjectDetectionNetwork(torch.nn.Module):
+    def __init__(self, transform, backbone, reg_head):
+        super().__init__()
+        self.transform = transform
+        self.backbone = backbone
+        self.reg_head = reg_head
+
+    @staticmethod
+    def compute_loss(detections, targets):
+        sum_diou_loss = 0
+        for det_target, target in zip(detections, targets):
+            diou = torchvision.ops.diou_loss.distance_box_iou_loss(det_target['boxes'], target['boxes'])
+            sum_diou_loss += diou
+        return sum_diou_loss / len(detections)
+
+    def forward(self, images, targets):
+        device = self.reg_head._parameters['bias'].device
+
+        origin_sizes = []
+        for image in images:
+            origin_sizes.append((image.shape[-2], image.shape[-1]))
+
+        images, targets = self.transform(images, targets)
+
+        detections = []
+        # return images, targets
+
+        boxes = self.reg_head(self.backbone(images.tensors.to(device)))
+        for box, (H, W) in zip(boxes, images.image_sizes):
+            box = box ** 2
+            box[2: 4] = box[2: 4] + box[0: 2]
+            box = box * torch.tensor([H, W, H, W], device=box.device)
+            d = {'boxes': box.reshape(1, -1), 'labels': torch.tensor([1])}
+            detections.append(d)
+
+        detections = self.transform.postprocess(detections, images.image_sizes, origin_sizes)
+        if self.training:
+            return self.compute_loss(detections, targets)
+        return detections
+
+
+def single_od_network():
+    image_mean = [0.0, 0.0, 0.0]
+    image_std = [1, 1, 1.0]
+    backbone = torchvision.models.resnet34()
+    backbone.fc = torch.nn.Identity()
+
+    from torchvision.models.detection.transform import GeneralizedRCNNTransform
+    config = {
+        'backbone': backbone,
+        'transform': GeneralizedRCNNTransform(min_size=224, max_size=224, image_mean=image_mean, image_std=image_std),
+        'reg_head': torch.nn.Linear(512, 4)
+    }
+    return SingleObjectDetectionNetwork(**config)
+
+
+def fasterrcnn_mobilenet_v3_large_320_fpn():
+    pretrained_model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_320_fpn(
+        weights=torchvision.models.detection.FasterRCNN_MobileNet_V3_Large_320_FPN_Weights.COCO_V1
+    )
+    out_channels = pretrained_model.backbone(torch.rand(1, 3, 1, 1))['0'].shape[1]
+
+    anchor_sizes = ((32, 64, 128, 256, 512,),) * 3
+    aspect_ratios = ((0.5, 1.0, 2.0),) * len(anchor_sizes)
+
+    defaults = {
+        "min_size": 320,
+        "max_size": 640,
+        "rpn_pre_nms_top_n_test": 10,
+        "rpn_post_nms_top_n_test": 1,
+        "rpn_pre_nms_top_n_train": 10,
+        "rpn_post_nms_top_n_train": 1,
+        "rpn_score_thresh": 0.05,
+    }
+    from torchvision.models.detection import FasterRCNN
+    from torchvision.models.detection.anchor_utils import AnchorGenerator
+
+    class DummyBackbone(torch.nn.Module):
+        # to fool FasterRCNN class
+        def __init__(self, out_channels):
+            super().__init__()
+            self.out_channels = out_channels
+
+    my_model = FasterRCNN(DummyBackbone(out_channels),
+                          num_classes=2,
+                          rpn_anchor_generator=AnchorGenerator(anchor_sizes, aspect_ratios),
+                          **defaults
+                          )
+    # use backbone in pretrained model directly
+    my_model.backbone = pretrained_model.backbone
